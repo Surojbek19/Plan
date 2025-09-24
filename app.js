6166de2b928d761101497bdaeb5be9756e4183e4
Sam <surojbeksv@gmail.com>
2025-09-24 12:40:14 +0900

    feat: MongoDB ulanish logic

---
+++ app.js
@@ -0,0 +1,46 @@
+console.log("Web Serverni boshlash");
+const express = require("express");
+const app = express();
+
+const fs = require("fs");
+
+// MongoDB chaqrish
+const db = require("./server").db();
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if(err) {
+        console.log("ERROR:", err);
+    } else {
+        user = JSON.parse(data)
+    }
+});
+
+// 1 Krish code
+app.use(express.static("public")); // user lar u-n public folder ochiq degani
+app.use(express.json()); // bu code .json foramtda kieib kelgan data ni Object holatga ozgartrib beradi
+app.use(express.urlencoded({extended: true})); //It alllows our Express app to read data from HTML forms(Post request), and automatically coverst into JS.
+//A Post request is a way for a client(like our browser or an app) to send data to a server. 
+
+// 2 Session code
+
+// 3 Views code (Backend da frontend ni yasash)
+
+app.set("views", "views"); // Backend da frontend ni yasash u-n folder kerak boladi. bu yerda osha folderni nomini ko'rsatib o'tyabmiz
+app.set("view engine", "ejs"); // bu yerda view engine "ejs" ekanligini ko'rsatdik
+
+// 4 Routing code
+app.post("/create-item", (req, res) => {
+    console.log(req);
+    res.json({test:"success"});
+});
+
+app.get("/author",(req, res) => {
+    res.render("author", {user: user});
+} )
+
+app.get("/", function(req, res) {
+    res.render("reja");
+})
+
+module.exports = app
--- server.js
+++ server.js
@@ -1,47 +1,29 @@
-console.log("Web Serverni boshlash");
-const express = require("express");
-const app = express();
+
+
 const http = require("http");
-const fs = require("fs");
+const mongodb = require("mongodb");
 
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-    if(err) {
-        console.log("ERROR:", err);
-    } else {
-        user = JSON.parse(data)
-    }
-});
+let db;
+const connectionString = "mongodb+srv://surojbeksv_db_user:adxjOec0mgjrhCIX@cluster0.esh6cit.mongodb.net/Reja";
 
-// 1 Krish code
-app.use(express.static("public")); // user lar u-n public folder ochiq degani
-app.use(express.json()); // bu code .json foramtda kieib kelgan data ni Object holatga ozgartrib beradi
-app.use(express.urlencoded({extended: true})); //It alllows our Express app to read data from HTML forms(Post request), and automatically coverst into JS.
-//A Post request is a way for a client(like our browser or an app) to send data to a server. 
+mongodb.connect(connectionString, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true}, (err, client) => {
+        if(err) console.log("ERROR on connection MongoDB");
+        else {
+            console.log("MongoDB connection succeed");
+            
+            module.exports = client;
+            const app = require("./app");
+            const server = http.createServer(app); //"app" expressm varibale nomi
+            let PORT = 3000;
+            server.listen(PORT, function() {
+                console.log(
+                    `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
 
-// 2 Session code
+                );
+            });
+        }
+    }) 
+    
 
-// 3 Views code (Backend da frontend ni yasash)
-
-app.set("views", "views"); // Backend da frontend ni yasash u-n folder kerak boladi. bu yerda osha folderni nomini ko'rsatib o'tyabmiz
-app.set("view engine", "ejs"); // bu yerda view engine "ejs" ekanligini ko'rsatdik
-
-// 4 Routing code
-app.post("/create-item", (req, res) => {
-    console.log(req);
-    res.json({test:"success"});
-});
-
-app.get("/author",(req, res) => {
-    res.render("author", {user: user});
-} )
-
-app.get("/", function(req, res) {
-    res.render("reja");
-})
-
-const server = http.createServer(app); //"app" expressm varibale nomi
-let PORT = 3000;
-server.listen(PORT, function() {
-    console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
-});
